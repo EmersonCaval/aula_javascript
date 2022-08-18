a990425f9602c2204f4afb22f5fc36ca19ebc19e
Cavalcanti Emerson <cavalcantiemersonsilva@gmail.com>
2022-08-17 21:24:41 -0300

    aula_javascript

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+
+var name = "Emerson Cavalcanti";
+var age = 23;
+var age2 = 10;
+var frase = "Brasil";
+
+//alert(name + " possui " + age + " anos!");
+//alert(age + age2);
+
+console.log(frase);
+console.log(age + age2);
+
+alert(frase.replace("Brasil", "Japão"));
