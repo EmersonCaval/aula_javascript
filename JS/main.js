7a31b565be7f61f5fc672987d88bcf405024c0af
Cavalcanti Emerson <cavalcantiemersonsilva@gmail.com>
2022-08-23 21:36:50 -0300

    AULA

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,79 @@
 
-var name = "Emerson Cavalcanti";
+
+function validaIdade(age){
+    var validar = true;
+    if (age >= 18){
+        validar =  true
+    }else{
+        validar = false
+    }
+}
+
+var age = prompt("Qual sua idade?" );
+console.log(validarIdade(agr));
+
+
+
+
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(phrase, name, new_name){
+  return phrase.replace(name, new_name)
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getSeconds());
+
+var count;
+for(count= 0; count <= 5; count++){
+    alert(count);
+};
+
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+};
+
+
+var age = prompt("How old are you?");
+//var age = 18
+if (age >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+
+var fruit1 = [{name: " Litter ", color: " Red "}, {name: " Grape ", color: "purple"}]
+console.log(fruit1);
+alert(fruit1[1].name);
+
+var fruit = {name: " Maça ", color:" Red "}
+console.log(fruit.name);*/
+
+
+//var list = ["maça", " pera " , "Laranja"];
+//list.push("uva");
+//list.pop("pera");
+//console.log(list.length);
+//console.log(list[1]);
+//console.log(list.reverse());
+//console.log(list);
+//console.log(list.toString());
+//console.log(list.join(" "));
+
+
+/*var name = "Emerson Cavalcanti";
 var age = 23;
 var age2 = 10;
 var frase = "Brasil";
@@ -10,4 +84,4 @@ var frase = "Brasil";
 console.log(frase);
 console.log(age + age2);
 
-alert(frase.replace("Brasil", "Japão"));
+alert(frase.replace("Brasil", "Japão"));*/
